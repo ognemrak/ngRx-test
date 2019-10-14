c7d1716ddb1004c116ef995e093e3ec1740bb433
User <user@Users-MacBook-Pro.local>
2019-10-14 21:20:21 +0300

    re-upd

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+declare const VERSION: string;
+import * as PDFkit from 'pdfkit';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'ngRx-test-app' + VERSION;
+
+  constructor() {}
+
+  onClick() {
+    console.log(1);
+    console.log(PDFkit);
+  }
+}
